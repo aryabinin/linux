c099b3c08de8c10e14f30e6cc861a493b1fd2d11
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-04-02 20:25:13 +0400

    mm: page_alloc: add kasan hooks on alloc and free paths
    Add kernel address sanitizer hooks to mark allocated page's addresses
    as accessible in corresponding shadow region.
    Mark freed pages as inaccessible.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

--- mm/kasan/kasan.c
+++ mm/kasan/kasan.c
@@ -247,6 +247,20 @@ static __always_inline void check_memory_region(unsigned long addr,
 	kasan_report(addr, size, write);
 }
 
+void kasan_alloc_pages(struct page *page, unsigned int order)
+{
+	if (likely(!PageHighMem(page)))
+		kasan_unpoison_shadow(page_address(page), PAGE_SIZE << order);
+}
+
+void kasan_free_pages(struct page *page, unsigned int order)
+{
+	if (likely(!PageHighMem(page)))
+		kasan_poison_shadow(page_address(page),
+				PAGE_SIZE << order,
+				KASAN_FREE_PAGE);
+}
+
 void __asan_load1(unsigned long addr)
 {
 	check_memory_region(addr, 1, false);
