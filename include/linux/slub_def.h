67c463d7a5297982834d8ee1067563f300ea414e
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-04-24 16:57:36 +0400

    mm: slub: share slab_err and object_err functions
    Remove static and add function declarations to mm/slab.h so they
    could be used by kernel address sanitizer.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

--- include/linux/slub_def.h
+++ include/linux/slub_def.h
@@ -115,4 +115,8 @@ static inline void *virt_to_obj(struct kmem_cache *s, void *slab_page, void *x)
 	return x - ((x - slab_page) % s->size);
 }
 
+void slab_err(struct kmem_cache *s, struct page *page, const char *fmt, ...);
+void object_err(struct kmem_cache *s, struct page *page,
+		u8 *object, char *reason);
+
 #endif /* _LINUX_SLUB_DEF_H */
