81642409cb21c5a5cf935ad0f16a57d4abb12049
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-09-24 14:08:55 +0400

    x86_64: add KASan support
    This patch adds arch specific code for kernel address sanitizer.
    
    16TB of virtual addressed used for shadow memory.
    It's located in range [0xffffd90000000000 - 0xffffe90000000000]
    which belongs to vmalloc area.
    
    At early stage we map whole shadow region with zero page.
    Latter, after pages mapped to direct mapping address range
    we unmap zero pages from corresponding shadow (see kasan_map_shadow())
    and allocate and map a real shadow memory reusing vmemmap_populate()
    function.
    
    Also replace __pa with __pa_nodebug before shadow initialized.
    __pa with CONFIG_DEBUG_VIRTUAL=y make external function call (__phys_addr)
    __phys_addr is instrumented, so __asan_load could be called before
    shadow area initialized.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

---
+++ arch/x86/include/asm/kasan.h
@@ -0,0 +1,28 @@
+#ifndef _ASM_X86_KASAN_H
+#define _ASM_X86_KASAN_H
+
+#define KASAN_SHADOW_START	(KASAN_SHADOW_OFFSET + (1ULL << 61) \
+					- (16ULL<<40))
+#define KASAN_SHADOW_END	(KASAN_SHADOW_START + (16ULL << 40))
+
+#ifndef __ASSEMBLY__
+
+extern pte_t zero_pte[];
+extern pte_t zero_pmd[];
+extern pte_t zero_pud[];
+
+extern pte_t poisoned_pte[];
+extern pte_t poisoned_pmd[];
+extern pte_t poisoned_pud[];
+
+#ifdef CONFIG_KASAN
+void __init kasan_map_zero_shadow(pgd_t *pgd);
+void __init kasan_init(void);
+#else
+static inline void kasan_map_zero_shadow(pgd_t *pgd) { }
+static inline void kasan_init(void) { }
+#endif
+
+#endif
+
+#endif
