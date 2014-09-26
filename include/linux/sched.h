1813afbf2c6143f2cbde9a2fa8128feb35ef0d8d
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-09-26 16:56:38 +0400

    UBSan: run-time undefined behavior sanity checker
    UBSan uses compile-time instrumentation to catch undefined behavior (UB).
    Compiler inserts code that perform certain kinds of
    checks before operations that could cause UB.
    If check fails (i.e. UB detected) __ubsan_handle_* function called.
    to print error message.
    
    So the most of the work is done by compiler.
    This patch just implements ubsan handlers printing errors.
    
    GCC supports this since 4.9, however upcoming GCC 5.0 has
    more checkers implemented.
    
    Different kinds of checks could be enabled via boot parameter:
    ubsan_handle=OEAINVBSLF.
    If ubsan_handle not present in cmdline default options are used: ELNVBSLF
    
    	O - different kinds of overflows
    	E - negation overflow, division overflow, division by zero.
    	A - misaligned memory access.
    	I - load from/store to an object with insufficient space.
    	N - null argument declared with nonnull attribute,
    	  returned null from function which never returns null, null ptr dereference.
    	V - variable size array with non-positive length
    	B - out-of-bounds accesses.
    	S - shifting out-of-bounds.
    	L - load of invalid value (value out of range for the enum type, loading other then 0/1 to bool type)
    	F - call to function through pointer with incorrect function type
    	    (AFAIK this is not implemented in gcc yet, probably works with clang, though
    	    	   I didn't check ubsan with clang at all).
    
    Instrumentation in kernel/printk/printk.c is disabled because struct printk_log is not properly aligned,
    therefore we are recursively taking logbuf_lock while trying to print error in __ubsan_handle*().
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1661,6 +1661,10 @@ struct task_struct {
 	unsigned int	sequential_io;
 	unsigned int	sequential_io_avg;
 #endif
+
+#ifdef CONFIG_UBSAN
+	unsigned int in_ubsan;
+#endif
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
