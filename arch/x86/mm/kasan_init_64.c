9fd5b89c81ed7fec26134639f75365921d7bbac6
Andrey Ryabinin <a.ryabinin@samsung.com>
2015-04-27 17:40:35 +0300

    x86: populate_zero_shadow
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

--- arch/x86/mm/kasan_init_64.c
+++ arch/x86/mm/kasan_init_64.c
@@ -55,9 +55,8 @@ static int __init zero_pte_populate(pmd_t *pmd, unsigned long addr,
 	pte_t *pte = pte_offset_kernel(pmd, addr);
 
 	while (addr + PAGE_SIZE <= end) {
-		WARN_ON(!pte_none(*pte));
-		set_pte(pte, __pte(__pa_nodebug(kasan_zero_page)
-					| __PAGE_KERNEL_RO));
+		set_pte(pte, pfn_pte(virt_to_pfn(kasan_zero_page),
+					PAGE_KERNEL_RO));
 		addr += PAGE_SIZE;
 		pte = pte_offset_kernel(pmd, addr);
 	}
@@ -71,24 +70,24 @@ static int __init zero_pmd_populate(pud_t *pud, unsigned long addr,
 	pmd_t *pmd = pmd_offset(pud, addr);
 
 	while (IS_ALIGNED(addr, PMD_SIZE) && addr + PMD_SIZE <= end) {
-		WARN_ON(!pmd_none(*pmd));
-		set_pmd(pmd, __pmd(__pa_nodebug(kasan_zero_pte)
-					| __PAGE_KERNEL_RO));
-		addr += PMD_SIZE;
-		pmd = pmd_offset(pud, addr);
+		pmd_populate_kernel(&init_mm, pmd, kasan_zero_pte);
+		addr = pmd_addr_end(addr, end);
+		if (addr >= end)
+			break;
+		pmd++;
 	}
+
 	if (addr < end) {
 		if (pmd_none(*pmd)) {
 			void *p = vmemmap_alloc_block(PAGE_SIZE, NUMA_NO_NODE);
 			if (!p)
 				return -ENOMEM;
-			set_pmd(pmd, __pmd(__pa_nodebug(p) | _KERNPG_TABLE));
+			pmd_populate_kernel(&init_mm, pmd, p);
 		}
 		ret = zero_pte_populate(pmd, addr, end);
 	}
 	return ret;
 }
-
 
 static int __init zero_pud_populate(pgd_t *pgd, unsigned long addr,
 				unsigned long end)
@@ -97,11 +96,12 @@ static int __init zero_pud_populate(pgd_t *pgd, unsigned long addr,
 	pud_t *pud = pud_offset(pgd, addr);
 
 	while (IS_ALIGNED(addr, PUD_SIZE) && addr + PUD_SIZE <= end) {
-		WARN_ON(!pud_none(*pud));
-		set_pud(pud, __pud(__pa_nodebug(kasan_zero_pmd)
-					| __PAGE_KERNEL_RO));
-		addr += PUD_SIZE;
-		pud = pud_offset(pgd, addr);
+		pud_populate(&init_mm, pud, kasan_zero_pmd);
+		zero_pmd_populate(pud, addr, pud_addr_end(addr, end));
+		addr = pud_addr_end(addr, end);
+		if (addr >= end)
+			break;
+		pud++;
 	}
 
 	if (addr < end) {
@@ -109,7 +109,7 @@ static int __init zero_pud_populate(pgd_t *pgd, unsigned long addr,
 			void *p = vmemmap_alloc_block(PAGE_SIZE, NUMA_NO_NODE);
 			if (!p)
 				return -ENOMEM;
-			set_pud(pud, __pud(__pa_nodebug(p) | _KERNPG_TABLE));
+			pud_populate(&init_mm, pud, p);
 		}
 		ret = zero_pmd_populate(pud, addr, end);
 	}
@@ -121,12 +121,13 @@ static int __init zero_pgd_populate(unsigned long addr, unsigned long end)
 	int ret = 0;
 	pgd_t *pgd = pgd_offset_k(addr);
 
-	while (IS_ALIGNED(addr, PGDIR_SIZE) && addr + PGDIR_SIZE <= end) {
-		WARN_ON(!pgd_none(*pgd));
-		set_pgd(pgd, __pgd(__pa_nodebug(kasan_zero_pud)
-					| __PAGE_KERNEL_RO));
-		addr += PGDIR_SIZE;
-		pgd = pgd_offset_k(addr);
+	while (IS_ALIGNED(addr, PGDIR_SIZE) && (addr + PGDIR_SIZE) <= end) {
+		pgd_populate(&init_mm, pgd, kasan_zero_pud);
+		zero_pud_populate(pgd, addr, pgd_addr_end(addr, end));
+		addr = pgd_addr_end(addr, end);
+		if (addr >= end)
+			break;
+		pgd++;
 	}
 
 	if (addr < end) {
@@ -134,13 +135,12 @@ static int __init zero_pgd_populate(unsigned long addr, unsigned long end)
 			void *p = vmemmap_alloc_block(PAGE_SIZE, NUMA_NO_NODE);
 			if (!p)
 				return -ENOMEM;
-			set_pgd(pgd, __pgd(__pa_nodebug(p) | _KERNPG_TABLE));
+			pgd_populate(&init_mm, pgd, p);
 		}
 		ret = zero_pud_populate(pgd, addr, end);
 	}
 	return ret;
 }
-
 
 static void __init populate_zero_shadow(const void *start, const void *end)
 {
