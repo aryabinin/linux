0b54f17e70ff50a902c4af05bb92716eb95acefe
Andrey Ryabinin <a.ryabinin@samsung.com>
2015-07-22 14:53:17 +0300

    arm: enable KASAN

--- arch/arm/kernel/module.c
+++ arch/arm/kernel/module.c
@@ -12,6 +12,7 @@
  */
 #include <linux/module.h>
 #include <linux/moduleloader.h>
+#include <linux/kasan.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
 #include <linux/elf.h>
@@ -40,9 +41,14 @@
 #ifdef CONFIG_MMU
 void *module_alloc(unsigned long size)
 {
-	void *p = __vmalloc_node_range(size, 1, MODULES_VADDR, MODULES_END,
+	void *p = __vmalloc_node_range(size, MODULE_ALIGN, MODULES_VADDR, MODULES_END,
 				GFP_KERNEL, PAGE_KERNEL_EXEC, 0, NUMA_NO_NODE,
 				__builtin_return_address(0));
+	if (p && (kasan_module_alloc(p, size) < 0)) {
+		vfree(p);
+		return NULL;
+	}
+
 	if (!IS_ENABLED(CONFIG_ARM_MODULE_PLTS) || p)
 		return p;
 	return __vmalloc_node_range(size, 1,  VMALLOC_START, VMALLOC_END,
