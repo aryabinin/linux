14cbc8511ff7f4377af13a505c6c1aedb341bde4
Andrey Ryabinin <a.ryabinin@samsung.com>
2015-07-24 17:23:40 +0300

    x86/kasan: generate KASAN_SHADOW_OFFSET in Makefile
    ARM64 has several different address space layouts and its
    going to have one more at least. Different address space layouts
    have different shadow offsets, so every new layout require adding
    new default value for CONFIG_KASAN_SHADOW_OFFSET.
    It's possible to generate KASAN_SHADOW_OFFSET in Makefile, so
    the shadow address for every possible layout will be auto-generated.
    
    However, we should do this in x86 too, because generic code
    depend on having CONFIG_KASAN_SHADOW_OFFSET.
    There is no functional changes here.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>

--- arch/x86/include/asm/kasan.h
+++ arch/x86/include/asm/kasan.h
@@ -1,16 +1,21 @@
 #ifndef _ASM_X86_KASAN_H
 #define _ASM_X86_KASAN_H
 
-/*
- * Compiler uses shadow offset assuming that addresses start
- * from 0. Kernel addresses don't start from 0, so shadow
- * for kernel really starts from compiler's shadow offset +
- * 'kernel address space start' >> KASAN_SHADOW_SCALE_SHIFT
- */
-#define KASAN_SHADOW_START      (KASAN_SHADOW_OFFSET + \
-					(0xffff800000000000ULL >> 3))
+#define KASAN_SHADOW_START      (0xffffec0000000000ULL)
 /* 47 bits for kernel address -> (47 - 3) bits for shadow */
 #define KASAN_SHADOW_END        (KASAN_SHADOW_START + (1ULL << (47 - 3)))
+
+/*
+ * This value is used to map an address to the corresponding shadow
+ * address by the following formula:
+ *	shadow_addr = (address >> 3) + KASAN_SHADOW_OFFSET;
+ *
+ * (1 << 61) shadow addresses - [KASAN_SHADOW_OFFSET,KASAN_SHADOW_END]
+ * cover all 64-bits of virtual addresses. So KASAN_SHADOW_OFFSET
+ * should satisfy the following equation:
+ *      KASAN_SHADOW_OFFSET = KASAN_SHADOW_END - (1ULL << 61)
+ */
+#define KASAN_SHADOW_OFFSET (KASAN_SHADOW_END - (1UL << (64 - 3)))
 
 #ifndef __ASSEMBLY__
 
