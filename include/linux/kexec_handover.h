e8bf4a167255daf847df233fee014de560af8b6f
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-08-07 01:44:10 +0000

    kho: allow to drive kho from within kernel
    Allow to do finalize and abort from kernel modules, so LUO could
    drive the KHO sequence via its own state machine.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>

--- include/linux/kexec_handover.h
+++ include/linux/kexec_handover.h
@@ -55,6 +55,10 @@ void kho_memory_init(void);
 
 void kho_populate(phys_addr_t fdt_phys, u64 fdt_len, phys_addr_t scratch_phys,
 		  u64 scratch_len);
+
+int kho_finalize(void);
+int kho_abort(void);
+
 #else
 static inline bool kho_is_enabled(void)
 {
@@ -110,6 +114,17 @@ static inline void kho_populate(phys_addr_t fdt_phys, u64 fdt_len,
 				phys_addr_t scratch_phys, u64 scratch_len)
 {
 }
+
+static inline int kho_finalize(void)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int kho_abort(void)
+{
+	return -EOPNOTSUPP;
+}
+
 #endif /* CONFIG_KEXEC_HANDOVER */
 
 #endif /* LINUX_KEXEC_HANDOVER_H */
