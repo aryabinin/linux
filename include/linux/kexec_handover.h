dce446db0bf8d807e42f4479254bf07da7ea1af8
Changyuan Lyu <changyuanl@google.com>
2025-08-07 01:44:13 +0000

    kho: add interfaces to unpreserve folios and physical memory ranges
    Allow users of KHO to cancel the previous preservation by adding the
    necessary interfaces to unpreserve folio.
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>
    Co-developed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>

--- include/linux/kexec_handover.h
+++ include/linux/kexec_handover.h
@@ -34,7 +34,9 @@ bool kho_is_enabled(void);
 bool is_kho_boot(void);
 
 int kho_preserve_folio(struct folio *folio);
+int kho_unpreserve_folio(struct folio *folio);
 int kho_preserve_phys(phys_addr_t phys, size_t size);
+int kho_unpreserve_phys(phys_addr_t phys, size_t size);
 struct folio *kho_restore_folio(phys_addr_t phys);
 int kho_add_subtree(const char *name, void *fdt);
 void kho_remove_subtree(void *fdt);
@@ -64,7 +66,17 @@ static inline int kho_preserve_folio(struct folio *folio)
 	return -EOPNOTSUPP;
 }
 
+static inline int kho_unpreserve_folio(struct folio *folio)
+{
+	return -EOPNOTSUPP;
+}
+
 static inline int kho_preserve_phys(phys_addr_t phys, size_t size)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int kho_unpreserve_phys(phys_addr_t phys, size_t size)
 {
 	return -EOPNOTSUPP;
 }
